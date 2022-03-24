763b45ce22ead3d2d728dc36fa563b2aeef586dc
Shruti Pant <shruti.panth7@gmail.com>
2022-03-24 17:05:18 +0000

    [LeetCode Sync] Runtime - 1 ms (99.82%), Memory - 57 MB (11.51%)

---
+++ mera-code/0169-majority-element/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int vote = 1;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] == candidate){
+                vote++;
+            } else {
+                vote--;
+            }
+            if(vote == 0){
+                candidate = nums[i];
+                vote = 1;
+            }
+        }
+        return candidate;
+    }
+}
