6b4b3e9c94d6f53a572049fd9f090af201506362
Shruti Pant <shruti.panth7@gmail.com>
2022-03-27 19:42:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (98.95%)

--- mera-code/0054-spiral-matrix/solution.java
+++ mera-code/0054-spiral-matrix/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
-        int m = matrix.length; //3
-        int n = matrix[0].length; //3
-        int minr = 0;
-        int maxr = m-1; //2
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int minr = 0; //Initialize min row, max row, min column and max column
+        int maxr = m-1;
         int minc = 0;
-        int maxc = n-1; //2
-        while(res.size() < m*n){
-            for(int i=minc; i<=maxc && res.size() < m*n; i++ ) {
+        int maxc = n-1;
+        while(res.size() < m*n){ //Condition to know when to stop i.e., when all elements have been traversed.
+            for(int i=minc; i<=maxc && res.size() < m*n; i++ ) {//res.size() < m*n prevent printing elements again when in innermost part of the matrix.
                 res.add(matrix[minc][i]);
             }
-            minr++;
+            minr++; //Increment min row so that the corner element won't be included in next loop.
             for(int i=minr; i<=maxr && res.size() < m*n; i++ ) {
                 res.add(matrix[i][maxc]);
             }
@@ -27,4 +27,6 @@ class Solution {
         }
         return res;
     }
+    // Credit: PepCoding youtube channel for detailed solution in Hindi. 
+    // Video link: https://www.youtube.com/watch?v=SVFXEqn3Ceo
 }
