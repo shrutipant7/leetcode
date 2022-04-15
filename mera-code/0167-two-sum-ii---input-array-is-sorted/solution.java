6d8dad3272ce05cf9ac95c4861f91d257c04e393
Shruti Pant <shruti.panth7@gmail.com>
2022-04-15 21:27:31 +0000

    [LeetCode Sync] Runtime - 1 ms (99.86%), Memory - 45.4 MB (99.93%)

---
+++ mera-code/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length,i=0,j=n-1;
+        while(i<j) {
+            int sum = numbers[i]+numbers[j];
+            if(sum == target) return new int[]{ i+1, j+1 };
+            else if(sum>target) j--;
+            else i++;
+        }
+        return null;
+    }
+}
