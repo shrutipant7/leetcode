456fbf9faf52bd48f526c4854faf2521cce24283
Shruti Pant <shruti.panth7@gmail.com>
2022-04-20 12:02:13 +0000

    [LeetCode Sync] Runtime - 15 ms (50.85%), Memory - 50.6 MB (9.82%)

---
+++ mera-code/0347-top-k-frequent-elements/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == k) return nums;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[k];
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+        for(int i=0; i< k; i++){
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
