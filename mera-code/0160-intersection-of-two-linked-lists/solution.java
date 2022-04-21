be644b3b192f2b29a0e7d59b4e912bebb8b2c328
Shruti Pant <shruti.panth7@gmail.com>
2022-04-21 07:16:13 +0000

    [LeetCode Sync] Runtime - 9 ms (16.53%), Memory - 44.7 MB (100.00%)

---
+++ mera-code/0160-intersection-of-two-linked-lists/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+         Map<ListNode, Integer> map = new HashMap<>();
+
+            while (headA != null) {
+                map.put(headA, map.getOrDefault(headA, 0) + 1);
+                headA = headA.next;
+            }
+
+            while (headB != null) {
+                if (map.containsKey(headB))
+                    return headB;
+                headB = headB.next;
+            }
+            return null;
+    }
+}
