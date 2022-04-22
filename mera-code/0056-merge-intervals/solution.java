b0a223ddbeb11b89214d455d73a5f5c9f8ee9664
Shruti Pant <shruti.panth7@gmail.com>
2022-04-22 20:10:31 +0000

    [LeetCode Sync] Runtime - 11 ms (17.59%), Memory - 56.1 MB (8.78%)

---
+++ mera-code/0056-merge-intervals/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length == 1) return intervals;
+        LinkedList<int[]> res= new LinkedList<>();
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        int[] compareInterval = intervals[0];
+        for(int i=0; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if(compareInterval[1] < interval[0]) {
+                res.add(compareInterval);
+                compareInterval = interval;
+            }
+            else {
+                compareInterval = new int[]{compareInterval[0], Math.max(interval[1], compareInterval[1])};
+            }
+        }
+        res.add(compareInterval);
+        return res.toArray(new int[res.size()][]);
+    }
+}
