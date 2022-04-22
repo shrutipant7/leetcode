b1219440c8a3d0a16d8f2d24366321f4ddb5dcc1
Shruti Pant <shruti.panth7@gmail.com>
2022-04-22 13:24:58 +0000

    [LeetCode Sync] Runtime - 8 ms (5.13%), Memory - 54.1 MB (5.31%)

--- mera-code/0143-reorder-list/solution.java
+++ mera-code/0143-reorder-list/solution.java
@@ -11,18 +11,18 @@
 class Solution {
     public void reorderList(ListNode head) {
         ListNode ptr = head;
-        Stack<ListNode> stack = new Stack<ListNode>();
-        while(ptr != null) {
-            stack.push(ptr); 
-            ptr=ptr.next;
+        Stack<ListNode> stack = new Stack<>();
+        while (ptr != null) {
+            stack.push(ptr);
+            ptr = ptr.next;
         }
         ptr = head;
-        while(ptr.next != null && ptr.next.next != null){
+        while(ptr != null && ptr.next != null) {
             ListNode top = stack.pop();
             stack.peek().next = null;
-            top.next=ptr.next; 
-            ptr.next=top; //Check if works or create object
-            ptr=ptr.next.next;
+            top.next = ptr.next;
+            ptr.next = top;
+            ptr = ptr.next.next;
         }
     }
 }
