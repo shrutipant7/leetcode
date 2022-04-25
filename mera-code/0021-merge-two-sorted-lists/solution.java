cf74ba813d56c140f50de534d5e3386dda49bd02
Shruti Pant <shruti.panth7@gmail.com>
2022-04-25 20:29:21 +0000

    [LeetCode Sync] Runtime - 1 ms (4.19%), Memory - 43.9 MB (95.22%)

--- mera-code/0021-merge-two-sorted-lists/solution.java
+++ mera-code/0021-merge-two-sorted-lists/solution.java
@@ -10,29 +10,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ptr = new ListNode();
-        ListNode tail = ptr;
-        if(list1 == null && list2 != null) return list2;
+        if(list1 == null && list2 == null) return null;
+        ListNode merged = new ListNode(0); //Dummy node
+        ListNode tail = merged;
         while(list1 != null && list2 != null) {
-            if(list1.val<=list2.val) {
-                tail.next=list1;
-                tail=tail.next;
-                list1=list1.next;
-            } 
-            else {
-                tail.next=list2;
-                tail=tail.next;
-                list2=list2.next;
+            if(list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
             }
+            else if(list2.val <= list1.val) {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
         }
         if(list1 != null) {
-                tail.next=list1;
-                tail=tail.next;
+            tail.next = list1;
         }
         if(list2 != null) {
-                tail.next=list2;
-                tail=tail.next;
+            tail.next = list2;
         }
-        return ptr.next;
+        return merged.next;
     }
 }
