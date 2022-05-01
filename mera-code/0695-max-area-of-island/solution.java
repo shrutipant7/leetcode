09b18cad7adda1431179d7ba3e18b4eecc0c3e6e
Shruti Pant <shruti.panth7@gmail.com>
2022-05-01 23:23:31 +0000

    [LeetCode Sync] Runtime - 4 ms (13.68%), Memory - 47.7 MB (7.51%)

---
+++ mera-code/0695-max-area-of-island/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for(int i=0; i< grid.length; i++) {
+            for(int j=0; j< grid[i].length; j++) {
+                if(grid[i][j] == 1){
+                   maxArea = Math.max(maxArea, dfs(grid, i, j));
+                }
+            }  
+        }
+        return maxArea;
+    }
+    
+    public int dfs(int[][] grid, int i, int j) {
+        if(i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] == 0)
+            return 0;
+        grid[i][j] = 0;
+        return 1+dfs(grid, i+1, j)+dfs(grid, i, j+1)+
+        dfs(grid, i-1, j)+dfs(grid, i, j-1);
+    }
+}
