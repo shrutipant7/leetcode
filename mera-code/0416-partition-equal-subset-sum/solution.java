79e45becd7e13a9a9be2055295bea6e16657504f
Shruti Pant <shruti.panth7@gmail.com>
2022-05-03 19:35:46 +0000

    [LeetCode Sync] Runtime - 82 ms (31.22%), Memory - 54.8 MB (43.50%)

--- mera-code/0416-partition-equal-subset-sum/solution.java
+++ mera-code/0416-partition-equal-subset-sum/solution.java
@@ -1,34 +1,23 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        int len = nums.length;
-        for(int n : nums) {
-            sum+=n;
+        for(int i=0; i<nums.length; i++) {
+            sum+=nums[i];
         }
-        if(sum%2!=0 ) {
-            return false;
-        }        
-        return subsetSum(nums, sum/2, len);
-    }
-    
-    static boolean subsetSum(int[] nums, int sum, int len) {
-        boolean[][] t = new boolean[len+1][sum+1];
-        for(int i=0; i<=len; i++) {
-            for(int j=0; j<=sum; j++) {
-                if(i == 0) t[i][j] = false;
-                if(j == 0) t[i][j] = true;
-            }
+        if(sum%2 != 0) return false;
+        boolean[][] dp = new boolean[nums.length+1][(sum/2)+1];
+        for(int i = 0; i<nums.length; i++){
+            for(int j = 0; j<=sum/2; j++){
+                if( j==0 ) dp[i][j] = true;
+                else if(i == 0) dp[i][j] = false;
+            } 
         }
-                             
-        for(int i=1; i<len+1; i++) {
-            for(int j=1; j<sum+1; j++) {
-                if(nums[i-1] <= j) {
-                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
-                } else  {
-                    t[i][j] = t[i-1][j];
-                }
-            }
+        for(int i = 1; i<=nums.length; i++){
+            for(int j = 1; j<=sum/2; j++){
+                if(nums[i-1] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+            } 
         }
-        return t[len][sum];
+        return dp[nums.length][sum/2];
     }
 }
