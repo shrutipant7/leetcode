52f742dda2818150cf384ad7df2a0ce4ad61a372
Shruti Pant <shruti.panth7@gmail.com>
2022-05-05 06:43:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (98.87%)

---
+++ mera-code/0098-validate-binary-search-tree/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        return BSTUtil(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    static boolean BSTUtil(TreeNode root, long min, long max){
+        if(root == null) return true;
+        if(root.val <= min || root.val >= max) return false;
+        return BSTUtil(root.left, min, root.val) && BSTUtil(root.right, root.val, max);
+    }
+}
