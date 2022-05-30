4b1dfd71d917ebf982b973631f2e3b135a6b76bf
Shruti Pant <shruti.panth7@gmail.com>
2022-05-30 16:10:12 +0000

    [LeetCode Sync] Runtime - 53 ms (6.77%), Memory - 58.7 MB (5.10%)

--- mera-code/0322-coin-change/solution.java
+++ mera-code/0322-coin-change/solution.java
@@ -1,24 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int len = coins.length;
-        int[][] dp = new int[len+1][amount+1];
-        
-        for(int i=0; i<=len; i++) {
-            dp[i][0] = 0;
-        }
-        for(int j=0; j<=amount; j++) {
-            dp[0][j] = Integer.MAX_VALUE-1;
-        }
-        for(int j=1; j<=amount; j++) {
-            if(j%coins[0] == 0) dp[1][j] = j/coins[0];
-            else dp[1][j] = Integer.MAX_VALUE-1;;
-        }
-        for(int i=2; i<=len; i++) {
-            for(int j=1; j<=amount; j++) {
-                if(coins[i-1] > j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = Math.min(dp[i-1][j], dp[i][j-coins[i-1]]+1);
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i=0; i<=coins.length; ++i) {
+            for(int j=0; j<=amount; ++j) { 
+                if(j==0) dp[i][j] = 0;
+                else if(i==0) dp[i][j] = 100000;
+                else {
+                    if(coins[i-1] > j) dp[i][j] = dp[i-1][j];
+                    else dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                }
             }
         }
-        return dp[len][amount] == Integer.MAX_VALUE-1 ? -1 : dp[len][amount];
+        return dp[coins.length][amount] > 10000 ? -1 : dp[coins.length][amount];
     }
 }
