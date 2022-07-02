33a1d68191861d1ddd7eb6b18b32c23ed4e27ff0
Shruti Pant <shruti.panth7@gmail.com>
2022-07-02 19:57:07 +0000

    [LeetCode Sync] Runtime - 2 ms (28.90%), Memory - 47.7 MB (11.40%)

--- mera-code/0057-insert-interval/solution.java
+++ mera-code/0057-insert-interval/solution.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] tempInterval = new int[intervals.length+1][2];
-        for(int i=0; i<intervals.length; i++) {
-            tempInterval[i] = intervals[i];
+        if(intervals.length == 0) return new int[][]{newInterval};
+        List<int[]> result = new ArrayList<>();
+        int i=0;
+        while(i < intervals.length && newInterval[0] > intervals[i][1]) {
+            result.add(intervals[i++]);
         }
-        tempInterval[tempInterval.length-1] = newInterval;
-        return merge(tempInterval);
-    }
-    static int[][] merge(int[][] intervals) {
-        if(intervals.length == 1) return intervals;
-        LinkedList<int[]> res= new LinkedList<>();
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-        int[] compareInterval = intervals[0];
-        for(int i=0; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            if(compareInterval[1] < interval[0]) {
-                res.add(compareInterval);
-                compareInterval = interval;
-            }
-            else {
-                compareInterval = new int[]{compareInterval[0], Math.max(interval[1], compareInterval[1])};
-            }
+        while(i < intervals.length && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        res.add(compareInterval);
-        return res.toArray(new int[res.size()][]);
+        result.add(newInterval);
+        while(i < intervals.length) {
+            result.add(intervals[i++]);
+        }
+        return result.toArray(new int[result.size()][]);
     }
 }
