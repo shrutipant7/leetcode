a8c930f88d4fe8300e9181cdfd53c82dc4cdf537
Shruti Pant <shruti.panth7@gmail.com>
2022-07-31 20:57:14 +0000

    [LeetCode Sync] Runtime - 2 ms (62.19%), Memory - 41.7 MB (100.00%)

---
+++ mera-code/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> inMap;
+    int curIndex = 0;
+    int[] preOrder;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preOrder = preorder;
+        inMap = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+        return dfs(0, preorder.length-1);
+    }
+    
+    public TreeNode dfs(int s, int e){
+        if(s>e) return null;
+        int curNode = preOrder[curIndex++];
+        TreeNode root  = new TreeNode(curNode);
+        int inRoot = inMap.get(curNode);
+        root.left = dfs(s, inRoot-1);
+        root.right = dfs(inRoot+1, e);
+        return root;
+    }
+}
