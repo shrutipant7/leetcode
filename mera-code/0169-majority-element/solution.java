00be341811cbf6fd8f4434e0f461d10b41a27269
Shruti Pant <shruti.panth7@gmail.com>
2024-08-10 19:30:23 +0000

    [LeetCode Sync] Runtime - 2 ms (72.19%), Memory - 53.7 MB (72.03%)

--- mera-code/0169-majority-element/solution.java
+++ mera-code/0169-majority-element/solution.java
@@ -1,18 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int majorityCount = (nums.length)/2;
         int candidate = nums[0];
         int vote = 1;
-        for(int i=0;i<nums.length;i++) {
-            if(nums[i] == candidate){
-                vote++;
-            } else {
+        for (int i:nums) {
+            if(candidate!=i) {
                 vote--;
+            } else {
+                vote++;
             }
-            if(vote == 0){
-                candidate = nums[i];
-                vote = 1;
+            if(vote == 0) {
+                candidate = i;
+                vote++;
             }
         }
-        return candidate;
+        // The element left in the end may or may not have majority. So check it.
+        // This extra code beats 75% but is not required as per question
+        int candCount = 0;
+        for (int i:nums) {
+            if(i==candidate) {
+                candCount++;
+            }
+        }
+        if (candCount > majorityCount) {
+            return candidate;
+        }
+        //No element has clear majority
+        else return -1;
     }
 }
