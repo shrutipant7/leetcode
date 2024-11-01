260b2e4f77f3ddd54944eda3eac317fe709fd611
Shruti Pant <shruti.panth7@gmail.com>
2024-11-01 16:12:42 +0000

    [LeetCode Sync] Runtime - 3 ms (92.37%), Memory - 43.6 MB (99.98%)

--- mera-code/0496-next-greater-element-i/solution.java
+++ mera-code/0496-next-greater-element-i/solution.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
         Map<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
-        for(int i=nums2.length-1; i>=0; i--) {
-                while(!stack.isEmpty() && stack.peek() <= nums2[i]) {
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = nums2.length-1; i>=0; i--) {
+            // if (stack.isEmpty()) {
+            //     map.put(nums2[i], -1);
+            //     stack.push(nums2[i]);
+            // }
+            // else if (stack.peek() < nums2[i]) {
+                while (!stack.isEmpty() && stack.peek() < nums2[i]) {
                     stack.pop();
                 }
-                if(stack.isEmpty()) {
-                    map.put(nums2[i], -1);
-                    stack.push(nums2[i]);
-                }
-                else {
-                    map.put(nums2[i], stack.peek());
-                    stack.push(nums2[i]);
-                }
+                if (stack.isEmpty()) map.put(nums2[i], -1);
+                else map.put(nums2[i], stack.peek());
+
+                stack.push(nums2[i]);
+            // } 
+            // else if (stack.peek() > nums2[i]) {
+            // else {
+            //     map.put(nums2[i], stack.peek());
+            //     stack.push(nums2[i]);
+            // }
         }
-        
-        for(int i=nums1.length-1; i>=0; i--) {
-            result[i] = map.get(nums1[i]);
+        for (int j=0; j< nums1.length; j++) {
+            nums1[j] = map.get(nums1[j]);
         }
-        return result;
+        return nums1;
     }
 }
