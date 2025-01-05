ab673eed6c86dd4358efcf99028b1092b1f12414
Shruti Pant <shruti.panth7@gmail.com>
2025-01-05 18:09:10 +0000

    [LeetCode Sync] Runtime - 11 ms (63.65%), Memory - 42.4 MB (99.96%)

---
+++ mera-code/0205-isomorphic-strings/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        //your code goes here
+        Map<Character,Character> map = new HashMap<>();
+
+
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+
+
+                // check if the value we are going to map
+                // isn't mapped to another value of s
+                if(map.containsValue(t.charAt(i))) return false;
+                map.put(s.charAt(i),t.charAt(i));
+            }else if( map.get(s.charAt(i)) != t.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
