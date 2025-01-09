a7005280da6ccaf754e5fe00c78d0df1f34ad373
Shruti Pant <shruti.panth7@gmail.com>
2025-01-09 19:44:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (99.95%)

---
+++ mera-code/0035-search-insert-position/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0; int right = nums.length-1;
+        int res = 0;
+        while(left <= right) {
+            int mid = (left+right)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                right = mid-1;
+            }
+            else if (nums[mid] < target) {
+                left = mid+1;
+                res = mid+1;
+            }
+        }
+        return res;
+    }
+}
