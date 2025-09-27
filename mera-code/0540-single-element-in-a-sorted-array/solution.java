6b09b87aaf1d8aefdbaf2fb68926b8924fbc25ef
Shruti Pant <shruti.panth7@gmail.com>
2025-09-27 08:37:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 50.1 MB (100.00%)

---
+++ mera-code/0540-single-element-in-a-sorted-array/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (mid % 2 != 0) {
+                if (nums[mid] != nums[mid - 1]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] != nums[mid + 1]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+        return nums[l];
+    }
+}
