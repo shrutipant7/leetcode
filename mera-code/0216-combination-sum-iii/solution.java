88c3d9747a417b9d60282564da16ffc53111f3f9
Shruti Pant <shruti.panth7@gmail.com>
2025-11-19 13:09:47 +0000

    [LeetCode Sync] Runtime - 1 ms (25.92%), Memory - 42.2 MB (87.46%)

--- mera-code/0216-combination-sum-iii/solution.java
+++ mera-code/0216-combination-sum-iii/solution.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(k, n, 1, res, new ArrayList<>());
+        int[] numArr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            numArr[i] = i + 1;
+        }
+        recFun(res, new ArrayList<>(), numArr, k, n, 0);
         return res;
     }
-    
-    static void helper(int k, int n, int index, List<List<Integer>> res, List<Integer> temp) {
-        if(temp.size() > k) {
+
+    private void recFun(
+            List<List<Integer>> res, List<Integer> temp, int[] numArr, int k, int n, int ind) {
+        if (n == 0 && k == temp.size()) {
+            res.add(new ArrayList<>(temp));
             return;
         }
-        
-        else if(temp.size() == k && n == 0) {
-            res.add(new ArrayList<>(temp));
-        }
-        
-        else {
-            for(int i=index; i<=9; i++){
-                temp.add(i);
-                helper(k, n-i, i+1, res, temp);
-                temp.remove(temp.size() - 1);
-            }
-        }
+        if (ind == numArr.length || n < 0 || k < temp.size())
+            return;
+        temp.add(numArr[ind]);
+        recFun(res, temp, numArr, k, n - numArr[ind], ind + 1);
+        temp.remove(temp.size() - 1);
+        recFun(res, temp, numArr, k, n, ind + 1);
     }
 }
