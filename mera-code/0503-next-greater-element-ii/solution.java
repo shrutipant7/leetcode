9a31cd9054210d408b50db46442f8ded1f75c0f2
Shruti Pant <shruti.panth7@gmail.com>
2025-11-24 08:21:54 +0000

    [LeetCode Sync] Runtime - 15 ms (54.93%), Memory - 48.4 MB (62.82%)

--- mera-code/0503-next-greater-element-ii/solution.java
+++ mera-code/0503-next-greater-element-ii/solution.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int len = nums.length;
-        int[] res = new int[len];
-        Deque<Integer> stack = new ArrayDeque<>();
-        //For circular array, iterate the array twice.
-        for (int i=(2*len)-1; i>=0; i--) {
-            while(!stack.isEmpty() && stack.peek() <= nums[i%len]) {
-                stack.pop();
-            }
-            //Only 2 possibilites from here: Either stack is empty after repeated popping or next greater element is found.
-            if(stack.isEmpty()) {
-                res[i%len] = -1;
-            } else {
-                res[i%len] = stack.peek();
-            }
-            stack.push(nums[i%len]);
+        int[] ans = new int[len];
+        Stack<Integer> st = new Stack<>();
+        for (int i = len - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums[i])
+                st.pop();
+            st.push(nums[i]);
         }
-        return res;
+        for (int i = len - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums[i])
+                st.pop();
+            ans[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i]);
+        }
+        return ans;
     }
 }
+
