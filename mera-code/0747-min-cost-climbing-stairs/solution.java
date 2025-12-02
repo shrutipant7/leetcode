765dae4e19d1b68993cb812103c133e60ec824c8
Shruti Pant <shruti.panth7@gmail.com>
2025-12-02 13:26:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.8 MB (81.69%)

---
+++ mera-code/0747-min-cost-climbing-stairs/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i=2; i<=n; i++) {
+            int cst = i == n ? 0 : cost[i];
+            dp[i] = Math.min(dp[i-1], dp[i-2])+cst;
+        }
+        return dp[n];
+    }
+}
