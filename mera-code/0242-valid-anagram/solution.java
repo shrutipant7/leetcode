25c366c4f3588463cef9790cdaa196b677a0fc7c
Shruti Pant <shruti.panth7@gmail.com>
2025-12-04 18:36:01 +0000

    [LeetCode Sync] Runtime - 6 ms (42.55%), Memory - 43.9 MB (98.98%)

--- mera-code/0242-valid-anagram/solution.java
+++ mera-code/0242-valid-anagram/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] list = new int[26];        
-        for(int i=0; i< s.length(); i++) {
-            list[s.charAt(i) - 'a'] = list[s.charAt(i) - 'a']+1;
+        if (s.length() != t.length())
+            return false;
+        int[] letters = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letters[s.charAt(i) - 'a']++;
+            letters[t.charAt(i) - 'a']--;
         }
-        for(int i=0; i< t.length(); i++) {
-            list[t.charAt(i) - 'a'] = list[t.charAt(i) - 'a']-1;
-        }
-        for(int i=0; i< list.length; i++) {
-        System.out.print(list[i]);
-            if(list[i] != 0) return false;
+        for (int i : letters) {
+            if (i != 0)
+                return false;
         }
         return true;
     }
