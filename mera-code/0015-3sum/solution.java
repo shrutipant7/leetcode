470ac335fb873cedd43852b75d3e3aaea5cff34f
Shruti Pant <shruti.panth7@gmail.com>
2025-12-07 11:39:47 +0000

    [LeetCode Sync] Runtime - 33 ms (69.91%), Memory - 59 MB (79.70%)

--- mera-code/0015-3sum/solution.java
+++ mera-code/0015-3sum/solution.java
@@ -1,32 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         int len = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
         for (int i = 0; i < len; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             int j = i + 1, k = len - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
+                if (sum < 0)
+                    j++;
+                else if (sum > 0)
+                    k--;
+                else {
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
                     temp.add(nums[k]);
-                    res.add(new ArrayList<>(temp));
+                    ans.add(new ArrayList<>(temp));
                     j++;
                     k--;
-                    while (j < k && nums[j - 1] == nums[j])
+                    while (j < k && nums[j] == nums[j - 1])
                         j++;
-                    while (j < k && nums[k + 1] == nums[k])
-                        k--;
-                } else if (sum < 0)
-                    j++;
-                else
-                    k--;
+                }
             }
         }
-        return res;
+        return ans;
     }
 }
