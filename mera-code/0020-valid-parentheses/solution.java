795f6106a15d354691e59f2b2824eea71369d318
Shruti Pant <shruti.panth7@gmail.com>
2025-12-08 19:13:03 +0000

    [LeetCode Sync] Runtime - 3 ms (87.71%), Memory - 43 MB (94.34%)

--- mera-code/0020-valid-parentheses/solution.java
+++ mera-code/0020-valid-parentheses/solution.java
@@ -2,19 +2,22 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && isMatching(stack.peek(), c)) {
-                stack.pop();
-            } else {
+            if (c == '(' || c == '[' || c == '{')
                 stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                if (!isMatching(stack.pop(), c))
+                    return false;
             }
         }
         return stack.isEmpty();
     }
 
     boolean isMatching(char c1, char c2) {
-        if((c1 == '(' && c2 == ')') ||
-        (c1 == '{' && c2 == '}') ||
-        (c1 == '[' && c2 == ']')) {
+        if ((c1 == '(' && c2 == ')') ||
+                (c1 == '{' && c2 == '}') ||
+                (c1 == '[' && c2 == ']')) {
             return true;
         }
         return false;
