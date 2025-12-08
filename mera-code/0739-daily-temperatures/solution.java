9b095af0feacafccd3144430d51e954b048d8cad
Shruti Pant <shruti.panth7@gmail.com>
2025-12-08 19:47:04 +0000

    [LeetCode Sync] Runtime - 77 ms (23.70%), Memory - 106.3 MB (73.38%)

--- mera-code/0739-daily-temperatures/solution.java
+++ mera-code/0739-daily-temperatures/solution.java
@@ -1,32 +1,18 @@
-class Solution {
+public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        // int len = temperatures.length;
-        // if(len == 1) return new int[]{0};
-        // int[] res = new int[len];
-        // Stack<Integer> stack = new Stack<>();
-        // for(int i = len-1; i>=0; i--) {
-        //     while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
-        //         stack.pop();
-        //     }
-        //     res[i] = stack.isEmpty() ? 0 : stack.peek()-i;
-        //     stack.push(i);
-        // }
-        // return res;
-        
         int len = temperatures.length;
-        if(len == 1) return new int[]{0};
         int[] res = new int[len];
-        int hottestDay = 0;
-        for(int i = len-1; i>=0; i--) {
-            if(temperatures[i] >= hottestDay) {
-                hottestDay=temperatures[i];
-                continue;
+        res[len - 1] = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(len - 1);
+
+        for (int i = len - 2; i >= 0; i--) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t >= temperatures[stack.peek()]) {
+                stack.pop();
             }
-            int days = 1;
-            while(temperatures[i+days] <= temperatures[i]) {
-                days+= res[i+days];
-            }
-            res[i] = days;
+            res[i] = stack.isEmpty() ? 0 : stack.peek()-i;
+            stack.push(i);
         }
         return res;
     }
