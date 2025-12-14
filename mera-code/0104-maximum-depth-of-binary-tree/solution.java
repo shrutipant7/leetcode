639ecfcc212fd1992a0810eb9fd11176ef3df4f3
Shruti Pant <shruti.panth7@gmail.com>
2025-12-14 12:53:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.4 MB (71.60%)

--- mera-code/0104-maximum-depth-of-binary-tree/solution.java
+++ mera-code/0104-maximum-depth-of-binary-tree/solution.java
@@ -14,27 +14,9 @@
  * }
  */
 class Solution {
-    // public int maxDepth(TreeNode root) {
-    //     if(root == null) return 0;
-    //     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    // }
-    
-    //Iterative BFS using queue
-     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        int depth = 0;
-        queue.offer(root); 
-        while(!queue.isEmpty()) {
-            depth++; //Increment depth for each level
-            int size = queue.size();
-            for(int i=0; i<size; i++) {
-                //All nodes of a level will be removed due to loop.
-                TreeNode node = queue.remove(); 
-                if(node.left != null) queue.offer(node.left);
-                if(node.right != null) queue.offer(node.right);
-            }
-        }
-        return depth;
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
