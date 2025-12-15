f2fc9dbac6eab41c0ebe85cae4d882d51d670212
Shruti Pant <shruti.panth7@gmail.com>
2025-12-15 14:22:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.7 MB (77.91%)

--- mera-code/0100-same-tree/solution.java
+++ mera-code/0100-same-tree/solution.java
@@ -15,10 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
+        if (p == null && q == null)
             return true;
-        }
-        
-         return p != null && q != null && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p == null || q == null)
+            return false;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
