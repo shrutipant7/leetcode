65eb9a903a8d555808aadfe72763f7e397266c27
Shruti Pant <shruti.panth7@gmail.com>
2025-12-15 18:00:55 +0000

    [LeetCode Sync] Runtime - 3 ms (66.44%), Memory - 46.6 MB (38.79%)

--- mera-code/0572-subtree-of-another-tree/solution.java
+++ mera-code/0572-subtree-of-another-tree/solution.java
@@ -15,29 +15,29 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) return false;
-        
-        // ----------  BFS   ---------
-        // Queue<TreeNode> q = new ArrayDeque<>();
-        // q.offer(root);
-        // while(!q.isEmpty()) {
-        //     TreeNode top = q.poll();
-        //     if(isSame(top, subRoot)) return true;
-        //     if(top.left != null) q.offer(top.left);
-        //     if(top.right != null) q.offer(top.right);
-        // }
-        // return false;
-        
-        // ----------  DFS  -----------
-        if(isSame(root, subRoot)) return true;
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        if (root == null && subRoot == null)
+            return true;
+
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode top = q.poll();
+            if (top.val == subRoot.val && treeMatched(top, subRoot))
+                return true;
+            if (top.left != null)
+                q.add(top.left);
+            if (top.right != null)
+                q.add(top.right);
+        }
+        return false;
+
     }
 
-     static boolean isSame(TreeNode subTreeNode, TreeNode subRoot) {
-        if(subTreeNode == null && subRoot == null) return true;
-        if(subTreeNode == null || subRoot == null) return false;
-        if(subTreeNode.val != subRoot.val) return false;
-        return isSame(subTreeNode.left, subRoot.left) 
-            && isSame(subTreeNode.right, subRoot.right);
+    public boolean treeMatched(TreeNode top, TreeNode subRoot) {
+        if (top == null && subRoot == null)
+            return true;
+        if (top == null || subRoot == null)
+            return false;
+        return top.val == subRoot.val && treeMatched(top.left, subRoot.left) && treeMatched(top.right, subRoot.right);
     }
 }
