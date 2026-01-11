8576dfe7d3cf47c8b3f4719ccb1ff0d99130414e
Shruti Pant <shruti.panth7@gmail.com>
2026-01-11 15:03:06 +0000

    [LeetCode Sync] Runtime - 3 ms (82.04%), Memory - 45.5 MB (69.39%)

--- mera-code/0120-triangle/solution.java
+++ mera-code/0120-triangle/solution.java
@@ -3,22 +3,22 @@ class Solution {
         int n = triangle.size();
         if (n == 1)
             return triangle.get(0).get(0);
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++)
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        dp[0][0] = triangle.get(0).get(0);
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = triangle.get(0).get(0);
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                int top = dp[i - 1][j];
-                int left = j == 0 ? Integer.MAX_VALUE : dp[i - 1][j - 1];
-                dp[i][j] = Math.min(top, left) + triangle.get(i).get(j);
+            int lim = triangle.get(i).size();
+            for (int j = lim - 1; j >= 0; j--) {
+                int top = dp[j];
+                int left = j == 0 ? Integer.MAX_VALUE : dp[j - 1];
+                dp[j] = Math.min(top, left) + triangle.get(i).get(j);
             }
         }
 
         int minSum = Integer.MAX_VALUE;
         for (int j = 0; j < n; j++) {
-            minSum = Math.min(minSum, dp[n - 1][j]);
+            minSum = Math.min(minSum, dp[j]);
         }
         return minSum;
     }
