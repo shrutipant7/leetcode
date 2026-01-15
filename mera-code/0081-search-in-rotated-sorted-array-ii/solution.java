92f65b523ad19d0eb6a7d6fba735146ceffb66fa
Shruti Pant <shruti.panth7@gmail.com>
2026-01-15 18:47:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.9 MB (71.99%)

---
+++ mera-code/0081-search-in-rotated-sorted-array-ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (target == nums[mid])
+                return true;
+            if (nums[l] == nums[mid] && nums[r] == nums[mid]) {
+                l++;
+                r--;
+                continue;
+            }
+            if (nums[l] <= nums[mid])
+                if (target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            else {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
