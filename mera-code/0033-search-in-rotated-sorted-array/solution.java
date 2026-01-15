3080a16b2dadf4476a28a99fd31dff6549f22f78
Shruti Pant <shruti.panth7@gmail.com>
2026-01-15 18:43:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (17.36%)

--- mera-code/0033-search-in-rotated-sorted-array/solution.java
+++ mera-code/0033-search-in-rotated-sorted-array/solution.java
@@ -1,27 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        while(low<=high) {
-            int mid = (low+high)/2;
-            if(nums[mid] == target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (target == nums[mid])
                 return mid;
-            } 
-            if(nums[low] <= nums[mid]){ //Left array is sorted
-                if(nums[low]<=target && nums[mid]>target) {
-                    high = mid-1;
-                }
-                else {
-                    low = mid +1;
-                }
-            }
-            else { //Right array is sorted
-                if(nums[high]>=target && nums[mid]<target) {
-                    low=mid+1;
-                }
-                else {
-                    high = mid-1;
-                }
+            if (nums[l] <= nums[mid])
+                if (target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            else {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
         return -1;
