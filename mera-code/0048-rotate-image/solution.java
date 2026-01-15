52a06c42ca9c67c0fc207194507bb7d150f217cd
Shruti Pant <shruti.panth7@gmail.com>
2026-01-15 08:39:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (21.97%)

--- mera-code/0048-rotate-image/solution.java
+++ mera-code/0048-rotate-image/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-//transpose the matrix
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if (i > j) {
-                    int temp = matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                    matrix[j][i]=temp;
-                }
+        //transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-// reverse columns of matrix. run loop only upto (n/2)-1 i.e., half
-// else replaced values will be replaced again hence remain unchanged.
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n/2; j++) {
-                    int temp = matrix[i][j];
-                    matrix[i][j]=matrix[i][n-1-j];
-                    matrix[i][n-1-j]=temp;
+        // reverse columns of matrix. run loop only upto (n/2)-1 i.e., half
+        // else replaced values will be replaced again hence remain unchanged.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
             }
         }
     }
