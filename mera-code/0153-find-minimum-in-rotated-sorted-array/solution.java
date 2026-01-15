31016e6d185bd25e36c5746d95f30e160b7c5446
Shruti Pant <shruti.panth7@gmail.com>
2026-01-15 16:46:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (63.53%)

--- mera-code/0153-find-minimum-in-rotated-sorted-array/solution.java
+++ mera-code/0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -1,34 +1,16 @@
 class Solution {
     public int findMin(int[] nums) {
-        int min = Integer.MAX_VALUE; 
-        int low = 0;
-        int high = nums.length-1;
-        while(low<=high) {
-            int mid = (low+high)/2;
-            if(nums[low] <= nums[mid]) {
-                min = Math.min(min, nums[low]);
-                low = mid+1;
-            }
-            else {
+        int min = 5001;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (nums[l] <= nums[mid]) {
+                min = Math.min(min, nums[l]);
+                l = mid + 1;
+            } else {
                 min = Math.min(min, nums[mid]);
-                high = mid-1;
+                r = mid - 1;
             }
-            
-            
-            
-            
-            
-            // if(nums[mid] < min) {
-            //     min = nums[mid];
-            // } 
-            // if(nums[low] < nums[mid]){ //left array is sorted 
-            //     min = Math.min(nums[low], min);
-            //     low = mid + 1;
-            // }
-            // else { //right array is sorted
-            //     min = Math.min(nums[high], min);
-            //     high = mid -1;
-            // }
         }
         return min;
     }
