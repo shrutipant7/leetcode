11f74e8e146939dc20209b70bf0e58fe989d2d32
Shruti Pant <shruti.panth7@gmail.com>
2026-01-17 19:00:30 +0000

    [LeetCode Sync] Runtime - 2 ms (94.34%), Memory - 72.5 MB (10.62%)

--- mera-code/0238-product-of-array-except-self/solution.java
+++ mera-code/0238-product-of-array-except-self/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
-        Arrays.fill(answer, 1);
-        int suffix = 1;
-        for(int i=1; i<n; i++) {
-            answer[i] = answer[i-1] * nums[i-1];
+        int len = nums.length;
+        int[] res = new int[len];
+        res[0] = 1;
+        for (int i = 1; i < len; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        for(int i=n-2; i>=0; i--) {
-            suffix*=nums[i+1];
-            answer[i] *=  suffix;
+        int mul = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            res[i] = res[i] * mul;
+            mul *= nums[i];
         }
-        return answer;
+        return res;
     }
 }
