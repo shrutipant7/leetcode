e0e7a06b91a245d0e61cc0237c5550b1211238fe
Shruti Pant <shruti.panth7@gmail.com>
2026-01-19 18:28:34 +0000

    [LeetCode Sync] Runtime - 1 ms (99.69%), Memory - 94.9 MB (8.45%)

--- mera-code/0121-best-time-to-buy-and-sell-stock/solution.java
+++ mera-code/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
+        int max = prices[prices.length - 1];
         int profit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < min)
-                min = prices[i];
+        for (int i = prices.length - 1; i >= 0; i--) {
+            if (prices[i] > max)
+                max = prices[i];
             else {
-                profit = Math.max(profit, prices[i] - min);
+                profit = Math.max(profit, max - prices[i]);
             }
         }
         return profit;
