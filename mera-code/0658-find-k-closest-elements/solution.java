64fd404c1265425ba0c566a9ed32e70184cb92b4
Shruti Pant <shruti.panth7@gmail.com>
2026-01-24 12:28:37 +0000

    [LeetCode Sync] Runtime - 4 ms (98.12%), Memory - 47.3 MB (97.05%)

--- mera-code/0658-find-k-closest-elements/solution.java
+++ mera-code/0658-find-k-closest-elements/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer> ans = new ArrayList<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] == b[0]) ? (b[1] - a[1]) : (b[0] - a[0]));
-        for (int n : arr) {
-            int[] temp = new int[] { Math.abs(n - x), n };
-            pq.add(temp);
-            if (pq.size() > k) {
-                pq.poll();
-            }
+        int len = arr.length;
+        int l = 0, r = len - 1;
+
+        while ((r - l + 1) > k) {
+            if (Math.abs(arr[l] - x) <= Math.abs(arr[r] - x))
+                r--;
+            else
+                l++;
         }
-        while (!pq.isEmpty()) {
-            ans.add(pq.poll()[1]);
+
+        for (int i = l; i <= r; i++) {
+            ans.add(arr[i]);
         }
-        Collections.sort(ans);
         return ans;
     }
 }
