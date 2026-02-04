224e2074b3445cb0a38c22120c2b4993bc2943c9
Shruti Pant <shruti.panth7@gmail.com>
2026-02-04 18:45:55 +0000

    [LeetCode Sync] Runtime - 13 ms (5.44%), Memory - 56.1 MB (95.67%)

--- mera-code/1544-count-good-nodes-in-binary-tree/solution.java
+++ mera-code/1544-count-good-nodes-in-binary-tree/solution.java
@@ -15,31 +15,32 @@
  */
 class Pair {
     TreeNode node;
-    int parent;
+    int max;
 
-    public Pair(TreeNode node, int parent) {
+    Pair(TreeNode node,
+            int max) {
         this.node = node;
-        this.parent = parent;
+        this.max = max;
     }
 }
 
 class Solution {
     public int goodNodes(TreeNode root) {
-        int ans = 0;
-        if (root == null)
-            return ans;
-
+        int maxAns = 0;
         Queue<Pair> q = new ArrayDeque<>();
         q.add(new Pair(root, root.val));
         while (!q.isEmpty()) {
             Pair top = q.poll();
-            if (top.node.val >= top.parent)
-                ans++;
-            if (top.node.left != null)
-                q.add(new Pair(top.node.left, Math.max(top.node.val, top.parent)));
-            if (top.node.right != null)
-                q.add(new Pair(top.node.right, Math.max(top.node.val, top.parent)));
+            if (top.node.val >= top.max) {
+                maxAns++;
+            }
+            if (top.node.left != null) {
+                q.add(new Pair(top.node.left, Math.max(top.max, top.node.left.val)));
+            }
+            if (top.node.right != null) {
+                q.add(new Pair(top.node.right, Math.max(top.max, top.node.right.val)));
+            }
         }
-        return ans;
+        return maxAns;
     }
 }
