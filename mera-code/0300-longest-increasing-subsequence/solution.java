f52ce2d0a1388aa00911acfe56a0946f28ae121c
Shruti Pant <shruti.panth7@gmail.com>
2026-02-07 21:47:41 +0000

    [LeetCode Sync] Runtime - 38 ms (66.49%), Memory - 46.3 MB (42.55%)

--- mera-code/0300-longest-increasing-subsequence/solution.java
+++ mera-code/0300-longest-increasing-subsequence/solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int maxSubSeq = 1;
+        int len = nums.length, maxLen = 0;
+        int[] dp = new int[len];
         Arrays.fill(dp, 1);
-        for(int i=nums.length-1; i>=0; --i) {
-            for(int j=i+1; j<nums.length; j++) {
-                if(nums[j] > nums[i]) 
-                    dp[i] = Math.max(dp[i], 1+dp[j]);
-                    maxSubSeq = Math.max(dp[i], maxSubSeq);
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
             }
         }
-        return maxSubSeq;
+        for (int it : dp)
+            maxLen = Math.max(maxLen, it);
+        return maxLen;
     }
 }
