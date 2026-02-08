cd338b64ab302f95e59341890e54c60bfcd6c53a
Shruti Pant <shruti.panth7@gmail.com>
2026-02-08 18:17:47 +0000

    [LeetCode Sync] Runtime - 1 ms (99.61%), Memory - 80.8 MB (5.22%)

--- mera-code/0053-maximum-subarray/solution.java
+++ mera-code/0053-maximum-subarray/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum = Integer.MIN_VALUE;
-        int curSum=0;
-        for (int i:nums) {
-            curSum+=i;
-            if (curSum > maxSum) maxSum = curSum;
-            if (curSum <= 0) curSum = 0;;
+        int curSum = 0;
+        for (int i : nums) {
+            curSum += i;
+            maxSum = Math.max(maxSum, curSum);
+            if (curSum <= 0)
+                curSum = 0;
         }
         return maxSum;
     }
