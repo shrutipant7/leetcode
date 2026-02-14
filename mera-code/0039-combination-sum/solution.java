b7f59f1502859a8df8897079b870db6308c9f1b5
Shruti Pant <shruti.panth7@gmail.com>
2026-02-14 19:50:22 +0000

    [LeetCode Sync] Runtime - 5 ms (10.27%), Memory - 46 MB (33.75%)

--- mera-code/0039-combination-sum/solution.java
+++ mera-code/0039-combination-sum/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, candidates, target, new ArrayList<>(), 0);
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        recFun(0, new ArrayList<>(), candidates, ans, target);
+        return ans;
     }
-    
-    static void backtrack(List<List<Integer>> res, int[] candidates, int target, List<Integer> temp, int start) {
-        if(target < 0) return; 
-        if(target == 0) {
-            res.add(new ArrayList<>(temp));
+
+    public void recFun(int ind, List<Integer> temp, int[] candidates, List<List<Integer>> ans, int target) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=start; i<candidates.length; i++) {
-                temp.add(candidates[i]);
-                backtrack(res, candidates, target-candidates[i], temp, i);
-                temp.remove(temp.size()-1);
-        }
+        if (target < 0 || ind == candidates.length)
+            return;
+        temp.add(candidates[ind]);
+        recFun(ind, temp, candidates, ans, target - candidates[ind]);
+        temp.remove(temp.size() - 1);
+        recFun(ind + 1, temp, candidates, ans, target);
     }
 }
