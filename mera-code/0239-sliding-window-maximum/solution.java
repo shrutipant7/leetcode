b94ea3742b783f1b6281941e5b08d2705515e188
Shruti Pant <shruti.panth7@gmail.com>
2026-02-16 18:31:56 +0000

    [LeetCode Sync] Runtime - 29 ms (84.62%), Memory - 147.8 MB (38.63%)

--- mera-code/0239-sliding-window-maximum/solution.java
+++ mera-code/0239-sliding-window-maximum/solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length, it = 0;
-        if (len == 1 || k == 1)
-            return nums;
-
         int[] ans = new int[len - k + 1];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (int i = 0; i < k; i++)
-            pq.add(new int[] { nums[i], i });
 
-        ans[it++] = pq.peek()[0];
-
-        for (int end = k; end < len; end++) {
-            while (pq.peek()[1] < end - k + 1)
-                pq.poll();
-            pq.add(new int[] { nums[end], end });
-            ans[it++] = pq.peek()[0];
+        Deque<int[]> dq = new ArrayDeque<>();
+        for (int end = 0; end < len; end++) {
+            while (!dq.isEmpty() && dq.peekLast()[0] <= nums[end]) {
+                dq.removeLast();
+            }
+            dq.addLast(new int[] { nums[end], end });
+            if (dq.peekFirst()[1] < end - k + 1) {
+                dq.removeFirst();
+            }
+            if (end >= k - 1)
+                ans[it++] = dq.peekFirst()[0];
         }
         return ans;
     }
